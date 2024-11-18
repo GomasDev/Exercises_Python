0c37da69ed588b2a03c90b0951fa1696e7cfd28e
Rafa Ballesteros Padial <ballepa01@gmail.com>
2024-11-18 19:38:41 +0100

    + exercises

--- calculatron2.0/function.py
+++ calculatron2.0/function.py
@@ -5,7 +5,7 @@ from PIL import Image
 import cv2
 
 #seleccion de operandos
-def operador():
+def operador()->str:
     operacion=random.randint(0,4)
     match operacion:
         case 0:
@@ -19,15 +19,12 @@ def operador():
         case 4:
             operacion="%"
     return operacion
+
 #operaciones
-def generarCuenta(numMin,numMax):
+def generarCuenta(numMin:int,numMax:int)->int:
     num1=random.randint(numMin,numMax)
     num2=random.randint(numMin,numMax)
     operacion=operador()
-    if operacion=="%" and operacion=="/":
-        while num2==0:
-            num2=random.randint(numMin,numMax)
-
     if operacion=="+":
         print(num1," + ",num2,"= ?")
         resultado = num1 + num2
@@ -44,19 +41,24 @@ def generarCuenta(numMin,numMax):
         print(resultado)
         return resultado
     elif operacion=="/":
+        if num2==0:
+            while num2==0:
+                num2 = random.randint(numMin, numMax)
         print(num1,"/",num2,"= ?" )
         resultado = num1 // num2
         print(resultado)
         return resultado
     elif operacion=="%":
+        if num2==0:
+            while num2==0:
+                num2 = random.randint(numMin, numMax)
         print(num1,"%",num2,"= ?" )
         resultado = num1 % num2
         print(resultado)
         return resultado
 
-
 #Menu Principal
-def imprimirMenu():
+def imprimirMenu()->int:
     print("====BIENVENIDO AL CALCULATRON 2.0====!!!!!")
     print("1.Jugar")
     print("2.Configuracion")
@@ -65,14 +67,16 @@ def imprimirMenu():
     print("0.Salir")
     seleccion=int(input("Selecciona una opcion: "))
     return seleccion
+
 #Menu configuracion
-def menuConfiguracion():
+def menuConfiguracion()->int:
     print("1.Cambiar numero de vidas")
     print("2.Cambiar numero minimo")
     print("3.Cambiar numero maximo")
     print("0.Salir")
     seleccion=int(input("¿Que desea hacer? "))
     return seleccion
+
 #generar imagen
 def mostrar_imagen(ruta,tiempo):
     imagen=Image.open(ruta)
@@ -99,7 +103,6 @@ def mostrar_video(ruta):
                 break
         else:
             break
-
     # Libera los recursos y cierra las ventanas
     cap.release()
     cv2.destroyAllWindows()
